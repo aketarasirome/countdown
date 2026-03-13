b22913d5ce8c29c5df233dabf212f04dfb442b46
Sirome Aketara <siromeaketara@SiromenoMacBook-Pro.local>
2026-03-13 10:22:19 +0900

    Add dynamic og image metadata to page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,27 +1,27 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from "next"
+import { Geist, Geist_Mono } from "next/font/google"
+import "./globals.css"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
-});
+})
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
-});
+})
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://countdown-00.vercel.app"),
   title: "Life Countdown",
   description: "Visualize how you spend the remaining hours of the year.",
-};
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="ja">
@@ -31,5 +31,5 @@ export default function RootLayout({
         {children}
       </body>
     </html>
-  );
+  )
 }
